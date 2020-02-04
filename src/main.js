93048b608c2faab7fbe3b5b6a023fddaff1bb1e9
march <lt19960223@gmail.com>
2020-02-04 15:50:32 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//解决移动端单击延迟问题
+import fastClick from 'fastclick'
+
+
+//css样式重置文件
+import './assets/css/reset.css'
+//解决移动端1px问题
+import './assets/css/border.css'
+
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
---
+++ src/views/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">我是Home</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components:{},
+  data(){
+    return {
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
